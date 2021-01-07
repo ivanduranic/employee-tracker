69e5ea8e4ea04566c4001685828597afd60237ab
Ivan Duranic <ivan.duranic@gmail.com>
2021-01-06 22:38:37 -0500

    wrote function add

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const inquirer = require("inquirer");
 
 //create my sql connection
 var connection = mysql.createConnection({
-    host: "localhost",
+  host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+  // Your port; if not 3306
+  port: 3306,
 
-    // Your username
-    user: "root",
+  // Your username
+  user: "root",
 
-    // Your password
-    password: "",
-    database: "firm_DB",
+  // Your password
+  password: "",
+  database: "firm_DB",
 });
 
 //connect
@@ -25,34 +25,59 @@ interactWithDB();
 connection.query = util.promisify(connection.query);
 
 function interactWithDB() {
-    inquirer
-        .prompt({
-            name: "action",
-            type: "rawlist",
-            message: "What would you like to do?",
-            choices: ["Add", "View", "Update", "Delete", "Exit"],
-        })
-        .then(function (answer) {
-            switch (answer.action) {
-                case "Add":
-                    add();
-                    break;
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: ["Add", "View", "Update", "Delete", "Exit"],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Add":
+          add();
+          break;
 
-                case "View":
-                    view();
-                    break;
+        case "View":
+          view();
+          break;
 
-                case "Update":
-                    update();
-                    break;
+        case "Update":
+          update();
+          break;
 
-                case "Delete":
-                    toDelete();
-                    break;
-                case "Exit":
-                    process.exit();
-                    break;
-            }
-        });
+        case "Delete":
+          toDelete();
+          break;
+        case "Exit":
+          process.exit();
+          break;
+      }
+    });
 }
 
+//Defining add function
+function add() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to add?",
+      choices: ["Department", "Role", "Employee"],
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Department":
+          addDepartment();
+          break;
+
+        case "Role":
+          addRole();
+          break;
+
+        case "Employee":
+          addEmployee();
+          break;
+      }
+    });
+}
